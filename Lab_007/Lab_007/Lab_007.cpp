d1b802d52eb369058d98348acb1ff8bdb92efad6
KroosT <anton.shestak.96@gmail.com>
2016-10-06 18:20:11 +0300

    Add draw train function.

--- Lab_007/Lab_007/Lab_007.cpp
+++ Lab_007/Lab_007/Lab_007.cpp
@@ -8,7 +8,7 @@
 LPCWSTR g_szClassName = L"myWindowClass";
 HINSTANCE hInst;
 HANDLE threads[3];
-HWND event_btn, cs_btn, start_btn, stop_btn;
+HWND hWnd, event_btn, cs_btn, start_btn, stop_btn;
 bool stop_flag = TRUE;
 bool thrds_created = FALSE;
 POINT coords;
@@ -18,6 +18,7 @@ bool previous_choice = FALSE;
 
 DWORD WINAPI MoveTrain_EventSynchronization(LPVOID lpParam);
 DWORD WINAPI MoveTrain_CriticalSection(LPVOID lpParam);
+void DrawTrain(HDC hdc, int x, int y);
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -25,17 +26,22 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	{
 		case WM_CREATE:
 		{
-			event_btn = CreateWindow(L"BUTTON", L"Event", WS_VISIBLE | WS_CHILD | BS_RADIOBUTTON, 10, 10, 60, 30,
+			event_btn = CreateWindow(L"BUTTON", L"Event", WS_VISIBLE | WS_CHILD | BS_RADIOBUTTON, 10, 30, 60, 30,
 				hwnd, (HMENU)ID_EVENT_BUTTON, hInst, NULL);
-			cs_btn = CreateWindow(L"BUTTON", L"Critical Section", WS_VISIBLE | WS_CHILD | BS_RADIOBUTTON, 10, 50, 120, 30,
+			cs_btn = CreateWindow(L"BUTTON", L"Critical Section", WS_VISIBLE | WS_CHILD | BS_RADIOBUTTON, 10, 70, 120, 30,
 				hwnd, (HMENU)ID_CS_BUTTON, hInst, NULL);
-			cs_btn = CreateWindow(L"BUTTON", L"Start", WS_VISIBLE | WS_CHILD | WS_BORDER, 200, 500, 100, 30,
+			start_btn = CreateWindow(L"BUTTON", L"Start", WS_VISIBLE | WS_CHILD | WS_BORDER, 200, 500, 100, 30,
 				hwnd, (HMENU)ID_START_BUTTON, hInst, NULL);
-			cs_btn = CreateWindow(L"BUTTON", L"Stop", WS_VISIBLE | WS_CHILD | WS_BORDER, 500, 500, 100, 30,
+			stop_btn = CreateWindow(L"BUTTON", L"Stop", WS_VISIBLE | WS_CHILD | WS_BORDER, 500, 500, 100, 30,
 				hwnd, (HMENU)ID_STOP_BUTTON, hInst, NULL);
 
 			SendMessage(event_btn, BM_SETCHECK, 1, NULL);
 			SendMessage(cs_btn, BM_SETCHECK, 0, NULL);
+
+			//Define start coords of the train.
+			coords.x = 5;
+			coords.y = 150;
+
 			break;
 		}
 		case WM_COMMAND:
@@ -132,7 +138,11 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			break;
 		case WM_PAINT:
 		{
-
+			HDC hdc = GetDC(hwnd);
+			TextOut(hdc, 10, 10, L"Switch synchronization method.", 
+				strlen("Switch synchronization method"));
+			TextOut(hdc, 10, 111, L"Warning! Avalible only when threads on pause!",
+				strlen("Warning! Avalible only when threads on pause!"));
 			break;
 		}
 		default:
@@ -184,6 +194,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		return 0;
 	}
 
+	hWnd = hwnd;
+
 	ShowWindow(hwnd, nCmdShow);
 	UpdateWindow(hwnd);
 
@@ -196,11 +208,58 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 }
 
 DWORD WINAPI MoveTrain_EventSynchronization(LPVOID lpParam) {
+	HDC hdc = GetDC(hWnd);
 
 	return 0;
 }
 
 DWORD WINAPI MoveTrain_CriticalSection(LPVOID lpParam) {
+	HDC hdc = GetDC(hWnd);
 
 	return 0;
+}
+
+void DrawTrain(HDC hdc, int x, int y)
+{
+	HPEN hPen = CreatePen(PS_SOLID, 2, RGB(0, 0, 0));
+	HBRUSH hBrush = CreateSolidBrush(RGB(200, 100, 0));
+
+	POINT train[11];
+
+	train[0].x = coords.x;
+	train[0].y = coords.y;
+
+	train[1].x = coords.x + 40;
+	train[1].y = coords.y;
+
+	train[2].x = coords.x + 40;
+	train[2].y = coords.y - 30;
+
+	train[3].x = coords.x + 60;
+	train[3].y = coords.y - 30;
+
+	train[4].x = coords.x + 60;
+	train[4].y = coords.y - 20;
+
+	train[5].x = coords.x + 70;
+	train[5].y = coords.y - 20;
+
+	train[6].x = coords.x + 70;
+	train[6].y = coords.y - 30;
+
+	train[7].x = coords.x + 80;
+	train[7].y = coords.y - 30;
+
+	train[8].x = coords.x + 80;
+	train[8].y = coords.y - 60;
+
+	train[9].x = coords.x;
+	train[9].y = coords.y - 60;
+
+	Polyline(hdc, train, 10);
+
+	hBrush = CreateSolidBrush(RGB(255, 0, 0));
+
+	Ellipse(hdc, coords.x + 10, coords.y - 50, coords.x + 30, coords.y - 70);
+	Ellipse(hdc, coords.x + 50, coords.y - 50, coords.x + 70, coords.y - 70);
 }
